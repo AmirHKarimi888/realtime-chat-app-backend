48f74e5419754fdd50f2450392efb8ccdbc93ca7
AmirHKarimi888 <amirhkarimi888@gmail.com>
2025-09-20 13:07:51 +0330

    test 3

--- server.js
+++ server.js
@@ -131,20 +131,18 @@ const server = http.createServer(app);
 // Enhanced CORS configuration - FIXED
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  
+
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie');
+  res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers'] || '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  
-  // Handle preflight requests
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
-  
+
   next();
 });
 
